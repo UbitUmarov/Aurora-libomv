6cbe223a5942747cd2decd00236c69250f2a7c09
UbitUmarov <ajlduarte@sapo.pt>
2011-02-28 03:42:26 +0000

    removed more "%256" wrongly used to mask a value to byte specially already typecasting to Byte

--- OpenMetaverse.StructuredData/StructuredData.cs
+++ OpenMetaverse.StructuredData/StructuredData.cs
@@ -746,10 +746,10 @@ namespace OpenMetaverse.StructuredData
         {
             this.value = new byte[]
             {
-                (byte)((value >> 24) % 256),
-                (byte)((value >> 16) % 256),
-                (byte)((value >> 8) % 256),
-                (byte)(value % 256)
+                (byte)((value >> 24)),
+                (byte)((value >> 16)),
+                (byte)((value >> 8)),
+                (byte)(value)
             };
         }
 
@@ -757,14 +757,14 @@ namespace OpenMetaverse.StructuredData
         {
             this.value = new byte[]
             {
-                (byte)((value >> 56) % 256),
-                (byte)((value >> 48) % 256),
-                (byte)((value >> 40) % 256),
-                (byte)((value >> 32) % 256),
-                (byte)((value >> 24) % 256),
-                (byte)((value >> 16) % 256),
-                (byte)((value >> 8) % 256),
-                (byte)(value % 256)
+                (byte)((value >> 56)),
+                (byte)((value >> 48)),
+                (byte)((value >> 40)),
+                (byte)((value >> 32)),
+                (byte)((value >> 24)),
+                (byte)((value >> 16)),
+                (byte)((value >> 8)),
+                (byte)(value)
             };
         }
 
@@ -772,14 +772,14 @@ namespace OpenMetaverse.StructuredData
         {
             this.value = new byte[]
             {
-                (byte)((value >> 56) % 256),
-                (byte)((value >> 48) % 256),
-                (byte)((value >> 40) % 256),
-                (byte)((value >> 32) % 256),
-                (byte)((value >> 24) % 256),
-                (byte)((value >> 16) % 256),
-                (byte)((value >> 8) % 256),
-                (byte)(value % 256)
+                (byte)((value >> 56)),
+                (byte)((value >> 48)),
+                (byte)((value >> 40)),
+                (byte)((value >> 32)),
+                (byte)((value >> 24)),
+                (byte)((value >> 16)),
+                (byte)((value >> 8)),
+                (byte)(value)
             };
         }
 
